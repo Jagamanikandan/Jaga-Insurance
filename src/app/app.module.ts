6705034178a4061db882975cb10d19191f9a4f6b
460833 <460833@01HW446875.India.TCS.com>
2017-09-26 20:31:02 +0530

    Delivering initial set-up for user & customer module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { CustomersComponent } from './customers/customers.component';
 import { QuoteIssueComponent } from './quote-issue/quote-issue.component';
 import { PolicyViewComponent } from './policy-view/policy-view.component';
+import { NewcustomerComponent } from './customers/newcustomer/newcustomer.component';
+import { CustomersearchComponent } from './customers/customersearch/customersearch.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +16,27 @@ import { PolicyViewComponent } from './policy-view/policy-view.component';
     UsersComponent,
     CustomersComponent,
     QuoteIssueComponent,
-    PolicyViewComponent
+    PolicyViewComponent,
+    NewcustomerComponent,
+    CustomersearchComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+    {
+      path: 'newcustomer',
+      component: NewcustomerComponent
+    },
+    {
+      path: 'customersearch',
+      component: CustomersearchComponent
+    },
+    {
+      path: 'quote-issue',
+      component: QuoteIssueComponent
+    }    
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
